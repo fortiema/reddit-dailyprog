13c06d7c96657ac6ecaed365b526fb2ecaaa20f9
Matt Fortier <fortiema@gmail.com>
2014-12-25 07:55:41 -0500

    Completed first part of #187-I

--- src/redditscheduler/PlannedActivity.java
+++ src/redditscheduler/PlannedActivity.java
@@ -15,7 +15,7 @@ import java.time.format.DateTimeFormatter;
  * Created on: 14-12-04.
  * Last Modified on: 14-12-04.
  */
-public class PlannedActivity {
+public class PlannedActivity implements Comparable<PlannedActivity> {
 
     private String name;
     private LocalDate date;
@@ -33,7 +33,7 @@ public class PlannedActivity {
     public PlannedActivity(String rawActivity) {
         String[] splitName = rawActivity.split("--");
         if (splitName.length == 2) {
-            this.name = splitName[1];
+            this.name = splitName[1].trim();
 
             try {
                 String[] splitDateTime = splitName[0].split(":",2);
@@ -51,6 +51,21 @@ public class PlannedActivity {
 
     }
 
+    public int compareTo(PlannedActivity a2) {
+        if (a2 == null) throw new NullPointerException();
+        if (this.getDate().isEqual(a2.getDate())) {
+            if (this.getStartTime().isBefore(a2.getStartTime())) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if (this.getDate().isBefore(a2.getDate())) {
+                return -1;
+        } else {
+            return 1;
+        }
+    }
+
     public String toString() {
         return (this.date.toString() + " - " + this.startTime.toString() + " to " + this.endTime.toString()
                 + " -- " + this.name);
@@ -62,6 +77,14 @@ public class PlannedActivity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 
     public LocalTime getStartTime() {
--- src/redditscheduler/RedditScheduler.java
+++ src/redditscheduler/RedditScheduler.java
@@ -3,9 +3,9 @@ package redditscheduler;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.*;
 
 /**
  * Title: RedditScheduler
@@ -22,19 +22,46 @@ import java.util.List;
  */
 public class RedditScheduler {
 
-    private static final String TIME_TABLE_FILE_PATH = "/Users/matt/Coding/Reddit-DailyProg/src/redditscheduler/TimeTable.txt";
-    private static ArrayList<PlannedActivity> plannedActivities = new ArrayList<PlannedActivity>();
+    private static final String TIME_TABLE_FILE_PATH = "G:\\Coding\\reddit-dailyprog\\src\\redditscheduler\\TimeTable.txt";
+
+    private static Map<LocalDate, List<PlannedActivity>> schedule = new TreeMap<LocalDate, List<PlannedActivity>>();
+
+    private static List<PlannedActivity> plannedActivities = new ArrayList<PlannedActivity>();
 
     public static void main(String[] args) {
 
+        // Recover contents of data file
         String[] rawActivities = processTimeTableFile(TIME_TABLE_FILE_PATH);
         if (rawActivities == null) { System.exit(-1); }
 
+        // Populate the dataset
         for (String s : rawActivities) {
             plannedActivities.add(new PlannedActivity(s));
         }
 
-        System.out.println(plannedActivities.get(2).toString() );
+        // Add activities to their date list in schedule map
+        for (PlannedActivity pa : plannedActivities) {
+            LocalDate date = pa.getDate();
+            if (!schedule.containsKey(date)) {
+                schedule.put(date, new ArrayList<PlannedActivity>());
+            }
+            schedule.get(date).add(pa);
+        }
+
+        // Add reditting
+        for (LocalDate date : schedule.keySet()) {
+            addRedditingInBiggestFreeGap(schedule.get(date));
+        }
+
+        for (LocalDate date : schedule.keySet()) {
+            System.out.println(date.toString());
+            System.out.println("---");
+            for (PlannedActivity act : schedule.get(date)) {
+                System.out.println(act.getStartTime() + " to " + act.getEndTime() + " - " + act.getName());
+            }
+            System.out.println();
+        }
+
         System.exit(0);
 
     }
@@ -49,8 +76,38 @@ public class RedditScheduler {
         }
     }
 
-    private static LocalDateTime findBiggestFreeGap(ArrayList<PlannedActivity> activities) {
-        return null;
+    private static void addRedditingInBiggestFreeGap(List<PlannedActivity> activities) {
+
+        // 1. Sort activities of the day by chronological order
+        Collections.sort(activities);
+
+        long maxFreeTimeSecs = 0;
+        long currFreeTimeSecs = 0;
+        PlannedActivity a1 = activities.get(0);
+        PlannedActivity a2 = activities.get(1);
+
+        // 2. Find largest free time gap in the day
+        for (int i = 0; i < activities.size()-1; i++) {
+            currFreeTimeSecs = Duration.between(activities.get(i).getEndTime(),
+                    activities.get(i + 1).getStartTime()).getSeconds();
+            if (currFreeTimeSecs > maxFreeTimeSecs) {
+                maxFreeTimeSecs = currFreeTimeSecs;
+                a1 = activities.get(i);
+                a2 = activities.get(i+1);
+            }
+        }
+
+        if (maxFreeTimeSecs > 0) {
+            // 3. Insert Redditing in largest free time gap
+            PlannedActivity redditing = new PlannedActivity();
+            redditing.setName("redditing");
+            redditing.setDate(a1.getDate());
+            redditing.setStartTime(a1.getEndTime());
+            redditing.setEndTime(a2.getStartTime());
+            activities.add(redditing);
+        }
+
+        Collections.sort(activities);
     }
 
 }
